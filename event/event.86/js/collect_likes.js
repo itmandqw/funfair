c34316f67b571dcd0c02aba5c8399a90ed580734
dengqiwei <itmandqw@163.com>
2018-01-24 17:56:42 +0800

    修改春节活动时间可以后台调整

--- event/event.86/js/collect_likes.js
+++ event/event.86/js/collect_likes.js
@@ -1,48 +1,33 @@
-// 获取时间的倒计时
-var begin = "2018/2/15 00:00:00";                  //设置活动开始时间
-var end = "2018/2/23 00:00:00";                    //设置活动结束时间
-
-$(function(){
-    var countObj = countTime(begin,end);
-    var earlyCount = countObj.earlyCount;
-    translateDate(earlyCount);
-
-})
-
-var timer = setInterval(function(){
-    var countObj = countTime(begin,end);
-    var earlyCount = countObj.earlyCount;
-    if(earlyCount<=0){
-        clearInterval(timer);
-    }
-    translateDate(earlyCount);
-},1000)
-
+//请求获得后台的开始时间，以及结束时间
+var begin = "";        //开始时间
+var end = "";          //结束时间
 
 angular.module("collectLikes",[])
-    .controller("collectLikesCtrl",["$location","$scope","$http",function($location,$scope,$http){
-        $scope.isLogin = false;    //用户登录状态
+    .controller("collectLikesCtrl",["$location","$scope","$http","$timeout",function($location,$scope,$http,$timeout){
+        $scope.isLogin = true;    //用户登录状态
         $scope.timing = false;     //活动进行状态
         $scope.err = false;        //初始状态
         $scope.timebefore = false; //倒计时显示状态
         $scope.win = false;        //活动结束，用户获奖状态
-        var obj = countTime(begin,end);
-        console.log(obj);
-        if(obj.earlyCount > 0){                          //活动还未开始，显示活动倒计时/活动未开始页面
-            $scope.timebefore = true;
-            // window.location.href = "../../../bs/common/not_start.html";
-        }
-        if(obj.earlyCount<=0 && obj.count > 0){          //活动正在进行，设置活动进行状态为true
-            $scope.timing = true;
-        }
 
         var params = $location.search();
         console.log(params)
         if(!params.token){
+
             $scope.isLogin = false;
+            $("#prompt").css("display","block");
         }else{
+
             $scope.isLogin = true;
             var token = params.token;
+
+            var time = getBeginEndTime(token); //获取到开始结束时间
+            begin = time.begins;
+            end = time.ends;
+            console.log("下面能用的",begin,end)
+
+            var obj = countTime(begin,end);
+            console.log(obj);
             if(obj.count <= 0){                    //活动已经结束,根据后台数据显示用户活动期间表现
                 console.log("用户已经登录，并且活动已经结束！",obj)
                 $http.post("../js/result.json",undefined,{
@@ -52,25 +37,25 @@ angular.module("collectLikes",[])
                     }
                 }).then(function(data){
                     if(data.data.success){
-                        // $("#win").css("display","block");
                         $scope.win = true;
                         $scope.num = data.data.num;
                         $scope.prize = data.data.prize;
                     }
                 },function(err){
-                    $scope.err = true;
+                    $("#busy").css("display","block");
                 })
             }
-        }
-        $scope.back = function(){
-            window.location.href = "about:blank";
-            window.close();
+            if(obj.earlyCount > 0){                          //活动还未开始，显示活动倒计时/活动未开始页面
+                $scope.timebefore = true;
+                // window.location.href = "../../../bs/common/not_start.html";
+            }
+            if(obj.earlyCount<=0 && obj.count > 0){          //活动正在进行，设置活动进行状态为true
+                $scope.timing = true;
+            }
         }
         $scope.close1 = function(){
-            $('#prompt').css("display","none");
-        }
-        $scope.close2 = function(){
-            $('#busy').css("display","none");
+            window.location.href = "about:blank";
+            window.close();
         }
         $scope.creat_mood = function(){
             console.log("创建足迹")
@@ -85,6 +70,25 @@ angular.module("collectLikes",[])
     }])
 
 
+//显示倒计时
+$(function(){
+    console.log("页面加载完毕的",begin,end)
+    var countObj = countTime(begin,end);
+    var earlyCount = countObj.earlyCount;
+    translateDate(earlyCount);
+})
+
+var timer = setInterval(function(){
+    var countObj = countTime(begin,end);
+    var earlyCount = countObj.earlyCount;
+    if(earlyCount<=0){
+        clearInterval(timer);
+    }
+    translateDate(earlyCount);
+},1000)
+
+
+
 //时间判断功能型函数
 function checkTime(time){
     if(time<10){
@@ -93,9 +97,7 @@ function checkTime(time){
     return time;
 }
 
-function countTime(beginTm,endTm){
-    var beginTime = new Date(beginTm).getTime();
-    var endTime = new Date(endTm).getTime();
+function countTime(beginTime,endTime){
     var nowTime = new Date().getTime();
     // 获取距离结束时间相差的秒数
     var count = (endTime - nowTime)/1000;
@@ -123,5 +125,27 @@ function translateDate(count){
     $("#mins").html(minutes);
 }
 
+
+function getBeginEndTime(token){
+    $.ajax({
+        type: "GET",
+        headers: {
+            "Authorization":"Bearer " + token
+        },
+        async: false,
+        url: "../js/detail.json",
+        dataType: "json",
+        success: function(data){
+            console.log(data)
+            begin = data.startTime;
+            end = data.endTime;
+        }
+    });
+    console.log("内部",begin,end)
+    return {
+        "begins": begin,
+        "ends": end
+    }
+}
 
 
